7feb6f660ba894e75b4f055f36b5e3730a9ef037
Jonathan <zhujiasheng@h5edu.cn>
2017-03-22 10:53:01 +0800

    android pack complete

--- mahjong_client/client/assets/scripts/HTTP.js
+++ mahjong_client/client/assets/scripts/HTTP.js
@@ -1,7 +1,9 @@
-//var URL = "http://139.129.247.226:9000";
-//var URL = "http://120.24.181.145:9000";
-//var URL = "http://120.24.59.70:9000";
-var URL = "http://192.168.1.121:9000";
+//local
+//var URL = "http://192.168.1.121:9000";
+//DEV SERVER
+var URL = "http://139.219.224.143:9000";
+//FORMAL SERVER
+//var URL = "http://192.168.1.121:9000";
 
 cc.VERSION = 20161227;
 var HTTP = cc.Class({
