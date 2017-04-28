b0381c719826071db6ea367a3da40ed646ecd25c
Jonathan <zhujiasheng@h5edu.cn>
2017-04-28 16:59:15 +0800

    完成
    Signed-off-by: Jonathan <zhujiasheng@h5edu.cn>

--- mahjong_client/client/assets/scripts/HTTP.js
+++ mahjong_client/client/assets/scripts/HTTP.js
@@ -1,10 +1,10 @@
 //local
 
-var URL = "http://192.168.1.121:9000";
+//var URL = "http://192.168.1.121:9000";
 //DEV SERVER
 //var URL = "http://139.219.224.143:9000";
 //FORMAL SERVER
-//var URL = "http://119.23.250.115:9000";
+var URL = "http://119.23.250.115:9000";
 
 cc.VERSION = 20161227;
 var HTTP = cc.Class({
