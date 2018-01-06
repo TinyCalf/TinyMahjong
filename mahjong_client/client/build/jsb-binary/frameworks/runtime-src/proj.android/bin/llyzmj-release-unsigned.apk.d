d44c2fa9fd56faf87dae3163afeb04146356ae47
Jonathan-ZHU <839560084@qq.com>
2018-01-06 20:09:38 +0800

    完成换包名，准备生成keystore

---
+++ mahjong_client/client/build/jsb-binary/frameworks/runtime-src/proj.android/bin/llyzmj-release-unsigned.apk.d
@@ -0,0 +1,10 @@
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\bin\llyzmj-release-unsigned.apk : \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\bin\llyzmj.ap_ \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\bin\classes.dex \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\libs\libPluginProtocol.jar \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\libs\android-support-v4.jar \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\libs\libammsdk.jar \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\libs\com.android.vending.expansion.zipfile.jar \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\libs\httpclient-4.4.1.1.jar \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\libs\android-async-http-1.4.9.jar \
+C:\Users\Jonathan\Desktop\TinyMahjong\mahjong_client\client\build\jsb-binary\frameworks\runtime-src\proj.android\libs\armeabi\libcocos2djs.so \
